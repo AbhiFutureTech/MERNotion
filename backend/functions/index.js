687dbc1a862e1cc3b638a3f32f9c1e6678fdb91f
Abhijit Patil <157373320+AbhiFutureTech@users.noreply.github.com>
2024-09-24 09:52:11 +0530

    Add files via upload

---
+++ backend/functions/index.js
@@ -0,0 +1,56 @@
+import "dotenv/config";
+
+import functions from "firebase-functions";
+import express from "express";
+import pageRouter from "./src/routes/page.js";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import tokenToID from "./src/middleware/authenticateToken.js";
+
+// initialise express server
+const server = express();
+
+server.get("/test", (req, res) => {
+  res.send({});
+});
+
+// solves undefined problem in POST requests
+server.use(express.json());
+
+// cors
+const allowedOrigins = [
+  "https://pettiboy.github.io",
+  "https://pseudo-notion.pettiboy.com",
+];
+server.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+// middleware
+server.use(tokenToID);
+
+// database
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); // connect to the database
+const db = mongoose.connection; // store connection
+db.on("error", (error) => console.error(error)); // error handling
+db.once("open", () => console.log("connected to db")); // log success message on successful connection
+
+// routers
+// map routes starting with '/page' to the router in 'routes/page.js'
+server.use("/page", pageRouter);
+
+export const api = functions.region("asia-south1").https.onRequest(server);
